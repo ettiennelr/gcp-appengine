fc6c0a466df2806bb92d786f26dd2330c7586529
ettienne <ettiennelr@spsi.co.za>
2018-02-02 06:46:17 +0200

    test

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -26,6 +26,7 @@ public class DemoServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
+
     resp.getWriter().println("{ \"name\": \"World\" }");
   }
 }
