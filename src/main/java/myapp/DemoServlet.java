2f1373b09ddcb66aff866595809fbfeda86b2b3c
ettienne <ettiennelr@spsi.co.za>
2018-02-07 14:10:14 +0200

    test

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -27,6 +27,6 @@ public class DemoServlet extends HttpServlet {
       throws IOException {
     resp.setContentType("text/plain");
 
-    resp.getWriter().println("{ \"name\": \"World Ettienne   22\" }");
+    resp.getWriter().println("{ \"name\": \"World Ettienne   100\" }");
   }
 }
